5d1bb2853d89e8177be45e4854045400c5d67c26
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-19 10:06:57 -0400

    core

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express from "express";
-
+import cors from "cors";
 import productRouter from "./routes/productRoutes";
 const app = express();
 
 // MIDDLEWARE
 app.use(express.json({ limit: "10kb" }));
-
+// Url Encoder, Allows express to parse data being sent from a form
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(
+  cors({
+    origin: "https://prn-product-inventor.netlify.app/",
+  })
+);
 // ROUTES
 app.use("/products", productRouter);
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to Our Express Server!!!!");
+  res.status(200).send("Welcome to Our Express Server");
 });
 
 export default app;
