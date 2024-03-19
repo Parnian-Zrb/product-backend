b34a4f7ecde1fff018cf019fd19a739a70d46421
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-19 12:15:10 -0400

    origin update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173/",
     // "https://prn-product-inventor.netlify.app/",
   })
 );
