46c57fcb013cb84f92bc4ccb9c26e70bb9282492
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-19 21:18:44 -0400

    delete

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,6 +8,11 @@ import {
 const router = express.Router();
 
 router.route("/").get(getAllProducts).post(addProduct);
+
 router.route("/:id").get(getProduct);
-router.route("/:id").delete(deleteProduct);
+// router.route("/:id").delete(deleteProduct);
+router.route("/:id").get(getProduct).delete(deleteProduct);
+
+router.route("/:addProduct").post(addProduct);
+
 export default router;
