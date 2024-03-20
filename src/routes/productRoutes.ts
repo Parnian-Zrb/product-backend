ca64b77820583f4983ea3aa4beb86a14e882fafe
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-19 20:48:52 -0400

    add

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,6 +8,5 @@ const router = express.Router();
 
 router.route("/").get(getAllProducts).post(addProduct);
 router.route("/:id").get(getProduct);
-router.route("/:id").delete(getProduct);
 
 export default router;
